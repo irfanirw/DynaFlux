e739f95589e34f887c4c9190928c1d723e8f62ef
Irfan Irwanuddin <irfanirwanuddin@gmail.com>
2025-12-22 00:16:35 +0800

    innitiate

--- DynaFlux/obj/Debug/net8.0-windows/DynaFlux.AssemblyInfo.cs
+++ DynaFlux/obj/Debug/net8.0-windows/DynaFlux.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DynaFlux")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdf1216bf37800a7c250c43cec5321431d8ebddb")]
 [assembly: System.Reflection.AssemblyProductAttribute("DynaFlux")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DynaFlux")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
