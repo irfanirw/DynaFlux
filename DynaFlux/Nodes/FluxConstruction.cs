e797c0366615d0b2d0920e23080e5db7d66a9ac4
Irfan Irwanuddin <irfanirwanuddin@gmail.com>
2025-12-22 00:59:45 +0800

    ok

--- DynaFlux/Nodes/FluxConstruction.cs
+++ DynaFlux/Nodes/FluxConstruction.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using FluxConstructionCore = DynaFluxCore.FluxConstruction;
+using FluxFenestrationConstructionCore = DynaFluxCore.FluxFenestrationConstruction;
 using FluxMaterialCore = DynaFluxCore.FluxMaterial;
 
 namespace DynaFlux.Nodes;
@@ -20,13 +21,22 @@ public static class FluxConstruction
         List<FluxMaterialCore> fluxMaterials,
         double uvalue)
     {
-        return new FluxConstructionCore
+        var construction = new FluxConstructionCore
         {
             Id = id,
             Name = name,
             FluxMaterials = fluxMaterials ?? new List<FluxMaterialCore>(),
+            // set a sensible default; may be overwritten below
             Uvalue = uvalue
         };
+
+        // If caller provided a non-positive uvalue (e.g. 0) compute it from materials
+        if (uvalue <= 0.0)
+        {
+            construction.Uvalue = DynaFluxCore.FluxUvalueCalculator.ComputeUValue(construction.FluxMaterials);
+        }
+
+        return construction;
     }
 
     /// <summary>
@@ -35,15 +45,26 @@ public static class FluxConstruction
     /// <param name="id">Construction identifier.</param>
     /// <param name="name">Construction name.</param>
     /// <param name="uvalue">Overall U-value.</param>
+    /// <param name="sc2">Shading coefficient 2.</param>
     /// <returns>Configured FluxConstruction.</returns>
-    public static FluxConstructionCore FenestrationByProperties(string id, string name, double uvalue)
+    public static FluxConstructionCore FenestrationByProperties(string id, string name, double uvalue, double sc2)
     {
-        return new FluxConstructionCore
+        var construction = new FluxFenestrationConstructionCore
         {
             Id = id,
             Name = name,
             FluxMaterials = new List<FluxMaterialCore>(),
-            Uvalue = uvalue
+            Uvalue = uvalue,
+            Sc2 = sc2
         };
+        // If uvalue not provided (<= 0) attempt to compute it. Fenestration often has no materials,
+        // so this will be a no-op in most cases but keeps behavior consistent.
+        if (uvalue <= 0.0)
+        {
+            construction.Uvalue = DynaFluxCore.FluxUvalueCalculator.ComputeUValue(construction.FluxMaterials);
+        }
+
+        construction.CalculateScTotal(construction.Sc1, sc2);
+        return construction;
     }
 }
